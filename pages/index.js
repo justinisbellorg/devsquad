cd292014e48ea9163b613496dea6ffc217b0c8e3
justin isbell <jisbell@us.ci.org>
2020-08-27 13:41:36 -0600

    adding tailwind start

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home ({ allPostsData }) {
         </p>
         <Link href ='/moreinfo'><a>More Info</a></Link>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -42,7 +42,7 @@ export default function Home ({ allPostsData }) {
 </li>
           ))}
         </ul>
-      </section>
+      </section> */}
     </Layout>
   )
 }
