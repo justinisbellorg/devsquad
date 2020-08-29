a2978888912797e623a919a11af86a463ef2da7c
Justin Isbell <justin@justinisbell.org>
2020-08-29 16:15:02 -0600

    eslint fixes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
 
-export async function getStaticProps() {
+export async function getStaticProps () {
   const allPostsData = getSortedPostsData()
   return {
     props: {
@@ -13,44 +11,60 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home ({ allPostsData }) {
+export default function Home () {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section>
-        <p>Devsquad is a site dedicated to helping software developers with skills they 
-          need in order to further their careers. Whether you are just starting out or are a seasoned
-          professional DevSquad exists to help you. This is an open source project as well so you can contribute to it to
-          get introduced to contributing to OSS
+        <p>
+          Devsquad is a site dedicated to helping software developers with
+          skills they need in order to further their careers. Whether you are
+          just starting out or are a seasoned professional DevSquad exists to
+          help you. This is an open source project as well so you can contribute
+          to it to get introduced to contributing to OSS
         </p>
       </section>
-      <section className='container m-auto'>
-            <h2 className='text-center'>I want to help people with:</h2>
-            <div className='container'>
-          <ul >
-              <li>Key Algorithms - conceptually how they work and implementations</li>
-              <li>Interview Kit - mock interview tools, sample questions, prep steps</li>
-              <li>Resume Tips - ways to help yourself honestly represent yourself and get a job where you can succeed</li>
-              <li>Sample Career Paths - What are some common paths you can take</li>
-              <li>How to grow your career - Regardless of your path, how can you keep growing?</li>
+      <section className="container m-auto">
+        <h2 className="text-center">I want to help people with:</h2>
+        <div className="container">
+          <ul>
+            <li>
+              Key Algorithms - conceptually how they work and implementations
+            </li>
+            <li>
+              Interview Kit - mock interview tools, sample questions, prep steps
+            </li>
+            <li>
+              Resume Tips - ways to help yourself honestly represent yourself
+              and get a job where you can succeed
+            </li>
+            <li>
+              Sample Career Paths - What are some common paths you can take
+            </li>
+            <li>
+              How to grow your career - Regardless of your path, how can you
+              keep growing?
+            </li>
           </ul>
-          </div>
-          <div>
-            <h2>Current Needs:</h2>
-            <ul>
-              <li>* Designer to help with look and Feel</li>
-              <li>* Start work on building Algorithm module components</li>
-            </ul>
-          </div>
-        <div className='text-center'>
-          <p>For now to get involved check out the {'\u00A0'}
-             
-          <a href='https://github.com/justinisbellorg/devsquad'>Github Repo</a> 
+        </div>
+        <div>
+          <h2>Current Needs:</h2>
+          <ul>
+            <li>* Designer to help with look and Feel</li>
+            <li>* Start work on building Algorithm module components</li>
+          </ul>
+        </div>
+        <div className="text-center">
+          <p>
+            For now to get involved check out the {'\u00A0'}
+            <a href="https://github.com/justinisbellorg/devsquad">
+              Github Repo
+            </a>
           </p>
         </div>
-        </section>
+      </section>
       {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
