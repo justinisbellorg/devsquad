2d4ad70fabc07d5d47436f0f4ab7d3871d6e7b64
Justin Isbell <justin@justinisbell.org>
2020-08-30 20:14:12 -0600

    fixed date import

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
-import Date from 'next/date'
+import Date from '../../components/date'
 export default function Post ({ postData }) {
   return (
     <Layout>
